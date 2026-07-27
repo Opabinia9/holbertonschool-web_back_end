eb4b4afd976b7e5eddbf05d978da644a23a6a981
Sebastion Price <sebastionprice@gmail.com>
2026-07-27 11:47:21 +1000

    Add 1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module: concat."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings.
+
+    Returns:
+        New concatenated string.
+
+    """
+    return str1 + str2
