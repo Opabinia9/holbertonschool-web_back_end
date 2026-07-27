a6257d0ada256489b976e89a355b52588fa7e662
Sebastion Price <sebastionprice@gmail.com>
2026-07-27 13:11:22 +1000

    Add 3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module: to_str."""
+
+
+def to_str(n: float) -> str:
+    """Convert float to string.
+
+    Returns:
+        String version of float passed.
+
+    """
+    return str(n)
