cdb2d2c7fbab34fa71b25d8273b8b9f1e4800bac
Sebastion Price <sebastionprice@gmail.com>
2026-08-11 15:56:35 +1000

    Fix next prev null again maybe

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -51,9 +51,10 @@ class Server:
 
         """
         cur_page = self.get_page(page, page_size)
-        prev_page = self.get_page(page - 1, page_size)
+        prev_page = (
+            self.get_page(page - 1, page_size) if page - 1 > 0 else None
+        )
         next_page = self.get_page(page + 1, page_size)
-        prev_page = prev_page if page - 1 > 0 else None
         next_page = next_page if next_page else None
         return {
             "page_size": page_size,
