828c395704582fd93280c11be0b8cdfd4ec44670
Sebastion Price <sebastionprice@gmail.com>
2026-08-31 14:53:23 +1000

    Add js-doc

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,3 +1,9 @@
+/**
+ * prepend string to each value in an array
+ * @param {Array} array
+ * @param {String} appendString
+ * @returns {Array}
+ */
 export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
     array[array.indexOf(value)] = appendString + value;
