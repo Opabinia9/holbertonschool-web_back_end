d5f79671956c6af3cfe9e55c999101cf58f83d5a
Sebastion Price <sebastionprice@gmail.com>
2026-09-01 12:39:52 +1000

    Add 1-stdin.js

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+const readline = require('readline-sync');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
